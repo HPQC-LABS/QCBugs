6da355aef42f331e8d82a36db16f45403d98cdf9
Nike Dattani <dattani.nike@gmail.com>
2023-07-12 00:41:05 -0400

    Further clean Issue024 input.py

--- Issue024/input.py
+++ Issue024/input.py
@@ -8,5 +8,5 @@ from pyscf.lib import logger
 neutralMolecule = pyscf.M(atom = [['O', (0,0,0)]], basis = 'STO-3G', verbose = 9, charge = 0, spin = 2)
 neutralMoleculeHF = scf.UHF(neutralMolecule).run()
 
-logObject = logger.new_logger(neutralMoleculeHF) # Creates a new logger object
-orbitals, _, stability, _  = neutralMoleculeHF.stability(return_status = True) # Returns four variables: Internal MO, External MO, Internal Stability, External Stability
+logObject = logger.new_logger(neutralMoleculeHF) 
+orbitals, _, stability, _  = neutralMoleculeHF.stability(return_status = True) 
